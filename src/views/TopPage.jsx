4133e22c9b06d3cac08f594c9cd98c3e0e409a96
MudasirBhat <bhatmuddu471@gmail.com>
2024-12-01 16:24:19 +0530

    adding to live

--- src/views/TopPage.jsx
+++ src/views/TopPage.jsx
@@ -1,7 +1,7 @@
 import myImg from "../assets/Images/myImg.png"
 import { Link } from "react-router-dom"
 // import Typed from "react-typed";
-import Typical from 'react-typical';
+// import Typical from 'react-typical';
 import "./home.css"
 function TopPage() {
     return (<>
@@ -9,11 +9,11 @@ function TopPage() {
             <div className="toppageMain container flex items-center justify-between">
                 <div className="top-left flex flex-col items-start justify-evenly w-1/2">
                     <p className="p1">{"Hi!, I am  a".toUpperCase()} </p>
-                    <p className="p2">
-                        <Typical
+                    <p className="p2">Web Developer
+                        {/* <Typical
                             loop={Infinity}
                             steps={['Web Developer', 2000, 'YouTuber', 2000]}
-                        />
+                        /> */}
                     </p>
                     <p className="p3 mb-3">Passionate web developer with 2 Years of experience in creating dynamic and visually appealing websites. Dedicated to delivering high-quality, user-friendly solutions to clients. Currently seeking new opportunities to collaborate and innovate in the ever-evolving world of web development.</p>
                     <div className=" mt-4 resumeBtn flex items-center justify-center ">
